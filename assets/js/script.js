4e255213cac93682e48712d37fb3c8e29a60c428
EsterDahri <esterdahri@gmail.com>
2023-09-05 15:00:12 +0200

    add html structure, link and js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/*
+##Traccia
+Sfruttiamo le timing functions (setInterval()) per fare il conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+
+##Consigli
+Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+Da quante ore è composto un giorno? //24h
+Da quanti minuti è composta un'ora? //60min
+Da quanti secondi è composto un minuto? //60s
+Da quanti millisecondi è composto un secondo? //1000ms
+Quanti millisecondi mi separano da domani alle 9:30? //1693899000000ms
+Esiste un oggetto JS in grado di gestire le date? Date()
+Esistono dei metodi per trasformare una data in millisecondi? //date.getTime();
+
+Tools:
+- data di domani
+- funzione set interval
+- metto in variabile la data di oggi
+- differenza tra data di oggi e domani
+- Math.floor formula su w3 school
+- queryselector per stampare in pagina
+*/
+
